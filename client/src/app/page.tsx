23156ae32a3fd8208522617be8728bee1e11581e
Olaotan Lawal <olaotan104@gmail.com>
2024-10-24 18:29:29 +0100

    Fixes fetch bug

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,42 +1,30 @@
 import Image from "next/image";
-import React from "react";
 import OrderDetails from './components/OrderDetails'
 import LookUp from './components/LookUp'
-import { GetServerSideProps } from 'next'
 
 async function fetchOrderNumber(orderId: string) {
-  const serviceUrl = process.env.SERVICE_URL
-  const response = await fetch(`${serviceUrl}/order?orderId=${orderId}`);
+  const serviceUrl = process.env.SERVICE_URL;
+  const response = await fetch(`${serviceUrl}/order?orderId=${orderId}`, {
+    cache: 'no-store',
+    next: { revalidate: 0 }
+  });
   if (response.ok) return response.json();
   return null;
 }
 
-type HomeProps = {
-  orderInformation: any | null;
-};
+async function Home({ searchParams }: { searchParams: { orderId?: string } }) {
+  const orderId = searchParams.orderId;
+  let orderInformation = null;
 
-export const getServerSideProps: GetServerSideProps<HomeProps> = async ({ query, res }) => {
-  res.setHeader(
-    'Cache-Control',
-    'no-cache, no-store, max-age=0, must-revalidate'
-  );
+  if (orderId) {
+    orderInformation = await fetchOrderNumber(orderId);
+  }
 
-  const orderId = query.orderId as string | undefined;
-  const orderInformation = orderId ? await fetchOrderNumber(orderId) : null;
-
-  return {
-    props: {
-      orderInformation,
-    },
-  };
-};
-
-const Home: React.FC<HomeProps> = ({ orderInformation }) => {
   if (!orderInformation) {
     return <LookUp />;
   }
 
   return <div><OrderDetails Details={orderInformation} /></div>;
-};
+}
 
 export default Home;
